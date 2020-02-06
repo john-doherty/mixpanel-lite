b0eb2cecdbcc4f47499159260df6d04476d69182
John Doherty <contact@johndoherty.info>
2020-02-06 00:42:33 +0000

    Replaced jsdom tests with puppeteer

--- tests/mixpanel-lite-spec.js
+++ tests/mixpanel-lite-spec.js
@@ -1,152 +1,191 @@
 'use strict';
 
-var nock = require('nock');
-var jsdom = require('jsdom');
 var path = require('path');
+var puppeteer = require('puppeteer');
+var querystring = require('querystring');
 
-var document = null;
-var window = null;
-
-// disable all outbound http requests
-nock.disableNetConnect();
+var url = 'file://' + path.join(__dirname, 'environment.html');
+var page = null;
+var browser = null;
 
 describe('mixpanel-lite', function () {
 
     // create a new browser instance before each test
     beforeEach(function (done) {
 
-        // remove all previous nocks
-        nock.cleanAll();
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
-        // have nock serve up the lib to the test page
-        nock('http://localhost:8080')
-            .get('/src/mixpanel-lite.js')
-            .replyWithFile(200, path.resolve('./src/mixpanel-lite.js'));
-
-        var virtualConsole = new jsdom.VirtualConsole();
-
-        var options = {
-            url: 'http://localhost:8080',
-            referrer: 'https://example.com/',
-            contentType: 'text/html',
-            runScripts: 'dangerously',
-            resources: 'usable',
-            userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
-            virtualConsole: virtualConsole.sendTo(console, { omitJSDOMErrors: true }) // redirect browser output to terminal
-        };
-
-        // load test page from disk (includes links to dependent scripts)
-        jsdom.JSDOM.fromFile(path.resolve(__dirname, 'environment.html'), options).then(function(dom) {
-
-            // expose the window/document object to tests
-            window = dom.window;
-            document = window.document;
-
-            // slight wait to allow scripts to load
-            setTimeout(function() {
-                expect(document).toBeDefined();
-                expect(document.title).toBe('Test Page');
-                done();
-            }, 250);
+        puppeteer.launch({
+            headless: true,
+            args: []
+        })
+        .then(function (item) {
+            browser = item;
+            return browser.newPage();
+        })
+        .then(function (item) {
+            page = item;
+            return page.goto(url);
+        })
+        .then(function() {
+            done();
         });
     });
 
-    it('should expose correct methods', function(done) {
-        expect(window.mixpanel).toBeDefined();
-        expect(window.mixpanel.init).toBeDefined();
-        expect(window.mixpanel.track).toBeDefined();
-        expect(window.mixpanel.reset).toBeDefined();
-        expect(window.mixpanel.identify).toBeDefined();
-        expect(window.mixpanel.people).toBeDefined();
-        expect(window.mixpanel.people.set).toBeDefined();
-        expect(window.mixpanel.mute).toBeDefined();
-        expect(window.mixpanel.unmute).toBeDefined();
-        done();
+    afterEach(function (done) {
+        browser.close().then(function() {
+            done();
+        });
     });
 
-    it('should sent data to /track endpoint', function(done) {
+    it('should expose correct methods', function (done) {
+
+        page.evaluate(function () {
+            return window.mixpanel;
+        })
+        .then(function (mixpanel) {
+            expect(mixpanel).toBeDefined();
+            expect(mixpanel.init).toBeDefined();
+            expect(mixpanel.track).toBeDefined();
+            expect(mixpanel.reset).toBeDefined();
+            expect(mixpanel.identify).toBeDefined();
+            expect(mixpanel.people).toBeDefined();
+            expect(mixpanel.people.set).toBeDefined();
+            expect(mixpanel.mute).toBeDefined();
+            expect(mixpanel.unmute).toBeDefined();
+            done();
+        });
+    });
+
+    it('should sent data to /track endpoint', function (done) {
 
         var now = (new Date()).getTime();
         var token = 'test-token-' + now;
         var eventName = 'test-event-' + now;
 
-        nock('https://api.mixpanel.com/').get('/track')
-            .query(function(query) {
-                expect(query).toBeDefined();
-                expect(query._).toBeDefined();
-                expect(query.data).toBeDefined();
-                expect(query.data).not.toEqual('');
+        page.setRequestInterception(true).then(function() {
+
+            // intercept ajax requests
+            page.on('request', function(request) {
+
+                var url = request.url();
+                var query = url.substr(url.indexOf('?') + 1);
+                var params = querystring.parse(query);
+
+                // be sure we've intercepted the correct URL
+                expect(url.startsWith('https://api.mixpanel.com/track')).toEqual(true);
+
+                // confirm it sent the correct params
+                expect(params).toBeDefined();
+                expect(params._).toBeDefined();
+                expect(params.data).toBeDefined();
+                expect(params.data).not.toEqual('');
 
                 // decode the data and convert to JSON object so we can inspect
-                var data = JSON.parse(Buffer.from(query.data, 'base64').toString('ascii'));
+                var data = JSON.parse(Buffer.from(params.data, 'base64').toString('ascii'));
 
+                // check the tracking data we sent is correct
                 expect(data.properties).toBeDefined();
                 expect(data.properties.distinct_id).toBeDefined();
-                expect(data.properties.$browser).toEqual('Safari');
+                expect(data.properties.$browser).toEqual('Chrome');
                 expect(data.properties.token).toEqual(token);
                 expect(data.event).toEqual(eventName);
 
                 done();
-            })
-            .reply(200);
+            });
 
-        window.mixpanel.init(token);
-        window.mixpanel.track(eventName);
+            // execute tracking (pass local vars into dom)
+            page.evaluate(function (t, e) {
+                window.mixpanel.init(t);
+                window.mixpanel.track(e);
+            }, token, eventName);
+        })
+        .catch(function(err) {
+            done(err);
+        });
     });
 
-    it('should sent data to /engage endpoint', function(done) {
+    it('should sent data to /engage endpoint', function (done) {
 
         var now = (new Date()).getTime();
         var token = 'test-token-' + now;
         var email = 'test-email-' + now + '@johndoherty.info';
 
-        nock('https://api.mixpanel.com').get('/engage')
-            .query(function(query) {
+        page.setRequestInterception(true).then(function() {
 
-                expect(query).toBeDefined();
-                expect(query._).toBeDefined();
-                expect(query.data).toBeDefined();
-                expect(query.data).not.toEqual('');
+            // intercept ajax requests
+            page.on('request', function(request) {
+
+                var url = request.url();
+                var query = url.substr(url.indexOf('?') + 1);
+                var params = querystring.parse(query);
+
+                // be sure we've intercepted the correct URL
+                expect(url.startsWith('https://api.mixpanel.com/engage')).toEqual(true);
+
+                // confirm it sent the correct params
+                expect(params).toBeDefined();
+                expect(params._).toBeDefined();
+                expect(params.data).toBeDefined();
+                expect(params.data).not.toEqual('');
 
                 // decode the data and convert to JSON object so we can inspect
-                var data = JSON.parse(Buffer.from(query.data, 'base64').toString('ascii'));
+                var data = JSON.parse(Buffer.from(params.data, 'base64').toString('ascii'));
 
                 expect(data.$distinct_id).toBeDefined();
                 expect(data.$set.$email).toEqual(email);
                 expect(data.$token).toEqual(token);
 
                 done();
-            })
-            .reply(200);
+            });
 
-        window.mixpanel.init(token);
-        window.mixpanel.people.set({ $email: email });
+            // execute tracking (pass local vars into dom)
+            page.evaluate(function (t, e) {
+                window.mixpanel.init(t);
+                window.mixpanel.people.set({ $email: e });
+            }, token, email);
+        })
+        .catch(function(err) {
+            done(err);
+        });
     });
 
-    it('should write request to localStorage first', function(done) {
+    it('should write tracking data to localStorage first', function (done) {
 
         var now = (new Date()).getTime();
         var token = 'test-token-' + now;
         var eventName = 'test-event-' + now;
 
-        // for API to reject even thus ensuring the data remains in local storage to test
-        nock('https://api.mixpanel.com/').get('/track').reply(500);
+        // intercept mixpanel API requests
+        page.setRequestInterception(true).then(function() {
 
-        // fire tracking event
-        window.mixpanel.init(token);
-        window.mixpanel.track(eventName);
+            // force ajax requests to fail
+            page.on('request', function(request) {
+                request.respond({ status: 500 });
+            });
 
-        // get data from local storage
-        var data = JSON.parse(window.localStorage.getItem('mixpanel-lite') || {});
+            // execute tracking (pass local vars into dom)
+            return page.evaluate(function (t, e) {
+                window.mixpanel.init(t);
+                window.mixpanel.track(e);
 
-        // check we have request info
-        expect(data).toBeDefined();
-        expect(Array.isArray(data)).toBe(true);
-        expect(data.length).toEqual(1);
-        expect(data[0].event).toEqual(eventName);
-        expect(data[0].properties.token).toEqual(token);
+                // return local storage so we can inspect it
+                return JSON.parse(window.localStorage.getItem('mixpanel-lite') || {});
+            }, token, eventName);
+        })
+        .then(function(data) {
 
-        done();
+            // check we have request info
+            expect(data).toBeDefined();
+            expect(Array.isArray(data)).toBe(true);
+            expect(data.length).toEqual(1);
+            expect(data[0].event).toEqual(eventName);
+            expect(data[0].properties.token).toEqual(token);
+
+            done();
+        })
+        .catch(function(err) {
+            done(err);
+        });
     });
-
 });
