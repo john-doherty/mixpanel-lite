f75791dc36bc9f7c8488dd598de87e6ae94ed309
John Doherty <contact@johndoherty.info>
2022-02-05 12:29:58 +0000

    Fixed eslint warning

--- tests/utils.js
+++ tests/utils.js
@@ -5,7 +5,7 @@
  * @returns {integer} random number
  */
 function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * ((max - min) + 1)) + min;
 }
 
 /**
